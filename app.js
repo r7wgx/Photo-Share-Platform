8942f31987a4729c1a53fc491708054c2a0b2d3d
Ramazan <r777wgx@gmail.com>
2023-03-28 16:06:51 +0300

    MVC

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 import express from "express";
 import dotenv from 'dotenv';
 import conn from './db.js';
-
-dotenv.config()
-
-// DataBase Connect
-
-conn();
+import pageRouter from "./routers/pageRouter.js";
 
 const app = express();
 const port = process.env.PORT;
 
-// ejs template engine
+dotenv.config() // dotenv integrated
+
+// DataBase Connection
+
+conn();
+
+// set template engine
 
 app.set("view engine", 'ejs')
 
 // static files middleware
+
 app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-    res.render('index');
-} )
+// Router
 
-app.get("/about", (req, res) => {
-    res.render('about');
-})
+app.use('/', pageRouter)
+
+
+// server
 
 app.listen(port, ()=> {
     console.log(`Server Run ${port}`);
