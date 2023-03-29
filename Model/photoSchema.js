2e1c809272d4cf28b987e1b4c1e0a0c2a216c17c
Ramazan <r777wgx@gmail.com>
2023-03-29 21:38:58 +0400

    Photo Schema

---
+++ Model/photoSchema.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const {Schema} = mongoose;
+
+const photoSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        tpye: String,
+        required: true,
+        trim: true
+    },
+    uploadedTime: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+
+const photo = mongoose.model("photoDB", photoSchema);
+
+export default photo;
