2616c5bc65b674ba84eaae4fca019542bd1d2e47
Ramazan <r777wgx@gmail.com>
2023-03-31 11:52:03 +0400

    register operations completed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import conn from './db.js';
 import pageRouter from "./routers/pageRouter.js";
 import photoRouter from "./routers/photoRouter.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
 const port = 3000;
@@ -21,12 +22,13 @@ app.set("view engine", 'ejs');
 
 app.use(express.static('public')); // static files middleware
 app.use(express.json()); // post isteyi zamani req body-den gelen datalari oxumaq ucun lazim olan middlaware
+app.use(express.urlencoded({extended: true})); // 
 
 // Router
 
 app.use('/', pageRouter);
 app.use('/photo', photoRouter);
-
+app.use('/users', userRouter);
 
 // server
 
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -10,4 +10,10 @@ const getAboutPage = (req, res) => {
     });
 }
 
-export {getAboutPage, getIndexPage}
+const getRegister = (req, res) => {
+    res.render('register', {
+        link: 'register' 
+    })
+}
+
+export {getAboutPage, getIndexPage, getRegister}
